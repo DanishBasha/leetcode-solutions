f83a966d758ee221bce0643aed890b2c0483faba
Danish Basha <danishbasha18@gmail.com>
2026-07-20 18:29:30 +0530

    Add A Number Between Two Others [A] from Codeforces

---
+++ 2225/A - A Number Between Two Others/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            long x = sc.nextLong();
+            long y = sc.nextLong();
+            
+            if (y == 2 * x) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+    }
+}
