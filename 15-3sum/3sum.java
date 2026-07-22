efd96ea62055b0224e774b64d5bc5d2ef2532468
Danish Basha <danishbasha18@gmail.com>
2026-07-22 14:26:37 +0530

    Time: 1955 ms (5.01%) | Memory: 64.6 MB (5.90%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 class Solution {
-     static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
+    //  static {
+    //     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+    //         try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+    //             fw.write("0");
+    //         } catch (Exception e) {
+    //         }
+    //     }));
+    // }
     public List<List<Integer>> threeSum(int[] nums) {
 
         List<List<Integer>> ans = new ArrayList<>();
