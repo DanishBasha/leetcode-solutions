bc01431abb47850f6929094dea899373dd98936f
Danish Basha <danishbasha18@gmail.com>
2026-07-27 11:28:56 +0530

    Time: 8 ms (99.30%) | Memory: 75.8 MB (88.76%) - LeetSync

---
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int dir [][] =  {{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}};
+    public int latestDayToCross(int R, int C, int[][] cells) {
+        int [] leader = new int[R*C+2];
+        boolean [] water = new boolean[R*C+2];
+        int days = 0;
+        for (int idx = 0;idx<R*C+2;idx++){
+            leader[idx] = idx;
+        }
+        for(int []cell : cells){
+            int row = cell[0];
+            int col = cell[1];
+            row--;
+            col--;
+            water[row*C+col+1] = true;
+            for (int diff[]: dir){
+                int nr = row+diff[0];
+                int nc = col+diff[1];
+               if (nr<R && nr >=0 && nc<C && nc>= 0 && water[nr*C+nc+1]){
+                 join(leader,row*C+col+1,nr*C+nc+1);
+               }
+            }
+            if (col == 0){
+                join(leader,0,row*C+col+1);
+            }else if(col == C-1){
+                join(leader,row*C+col+1,R*C+1);
+            }
+            if(find(leader,0) == find(leader,R*C+1)){
+                break;
+            }
+            days++;
+        }
+        return days;
+    }
+    private int find(int leader[],int node){
+            if (leader[node] != node){
+                leader[node] = find(leader,leader[node]);
+            }
+            return leader[node];
+    }
+    private void join (int leader[],int left,int right){
+       leader[find(leader,right)] = find(leader,left); 
+    }
+}
