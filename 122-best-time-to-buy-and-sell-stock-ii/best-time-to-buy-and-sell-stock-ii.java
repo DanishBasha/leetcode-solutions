27998a71c0a48bfebb4ba376b64fdbf5e9e7d3ce
Danish Basha <danishbasha18@gmail.com>
2026-08-04 11:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (84.91%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+     int profit =0;
+
+     for(int idx = 1;idx<prices.length;idx++){
+        if(prices[idx] > prices[idx-1]){
+            profit += prices[idx]-prices[idx-1]; 
+        }
+     }   
+     return profit;
+    }
+}
